585cb04e1fe798d0b5be89495a3cad16642df429
VillalobosLobos <villalobossanchezezequiel61@gmail.com>
2025-10-06 15:13:20 -0600

    Delete rutas directory

--- rutas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('' , views.index , name='index'),
-    path('terminadas/' , views.entregasTerminadas, name='terminadas'),
-    path('rutas/' , views.rutasPorHacer , name='rutas'),
-    path('entrega/<int:id_entrega>/eliminar/', views.eliminarEntrega, name='eliminar_entrega')
-]
